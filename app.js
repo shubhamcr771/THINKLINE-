751a928c871d564861c63fd884858ee55b306f1a
Shubham <shubhamsingh705010@gmail.com>
2024-05-10 13:05:40 +0530

    format

--- app.js
+++ app.js
@@ -1,62 +1,52 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
-
 const port = 3000;
 const app = express();
 
-
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
-// GET REQUEST 
+// GET REQUEST
 
 app.get("/", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 app.get("/views/home.ejs", (req, res) => {
-    res.render("home");
+  res.render("home");
 });
 
 app.get("/create-post", (req, res) => {
-    res.render("create-post");
+  res.render("create-post");
 });
-
 
 app.get("/edit-post/:title/:content", (req, res) => {
-    const title = req.params.title;
-    const content = req.params.content;
-    res.render("edit-post", { title, content });
+  const title = req.params.title;
+  const content = req.params.content;
+  res.render("edit-post", { title, content });
 });
 
-
-
-
-// POST REQUEST 
+// POST REQUEST
 
 app.post("/delete-post", (req, res) => {
-    // Delete the post from the database
-    // Redirect the user to the home page or a confirmation page
-    res.redirect("/");
+  // Delete the post from the database
+  // Redirect the user to the home page or a confirmation page
+  res.redirect("/");
 });
 
-
 app.post("/edit-post", (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    res.render("view-post", {title, content})
+  const title = req.body.title;
+  const content = req.body.content;
+  res.render("view-post", { title, content });
 });
 
 app.post("/create-post", (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    res.render("view-post", {title, content});
-
+  const title = req.body.title;
+  const content = req.body.content;
+  res.render("view-post", { title, content });
 });
 
 app.listen(port, (req, res) => {
-    console.log(`App is running on port: ${port}`);
+  console.log(`App is running on port: ${port}`);
 });
