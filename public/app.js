e23409ba279f46922c2cc5e6cb8c03b44aeff1ae
Shubham <shubhamsingh705010@gmail.com>
2024-05-11 13:20:44 +0530

    next time

--- public/app.js
+++ public/app.js
@@ -14,6 +14,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 // GET REQUEST
 
